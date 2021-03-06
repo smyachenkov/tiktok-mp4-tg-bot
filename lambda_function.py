83c42fa6043f73601af6ed8d1670c765cd0d95f7
smyachenkov <s.myachenkov@gmail.com>
2021-03-06 21:13:37 +0300

    input validation

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ import logging
 
 from telegram_client import TelegramClient
 from tiktok import download_video
-from tiktok import convert_to_gif
+from tiktok import strip_url_params
 from tiktok import is_tiktok_url
 
 logger = logging.getLogger()
@@ -27,13 +27,19 @@ def process_even(event):
 
     text = message['message']['text']
 
+    if text == "/start":
+        telegram_client.send_message(chat_id, "Hi! Send me a TikTok link")
+        return
+
     if not is_tiktok_url(text):
         telegram_client.send_message(chat_id, "Please send valid TikTok url")
         return
 
-    telegram_client.send_message(chat_id, "Converting your TikTok, please wait a moment")
-    video_file = download_video(text)
-    # gif_file = convert_to_gif(video_file)
+    url = strip_url_params(text)
+    logger.info("Downloading video from %s", url)
+
+    telegram_client.send_message(chat_id, "Downloading your TikTok, please wait a moment")
+    video_file = download_video(url)
     telegram_client.send_file(chat_id, video_file)
 
 
--- tiktok.py
+++ tiktok.py
@@ -1,6 +1,7 @@
 import subprocess
 import logging
 import os
+from urllib.parse import urlparse
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -9,9 +10,14 @@ VIDEO_OUTPUT_DIR = "/tmp/tiktok/"
 TIKTOK_SCRAPER_CMD = "/tmp/node_modules/.bin/tiktok-scraper video {} -d --filepath {}"
 
 
-def is_tiktok_url(url):
-    # todo: implement
-    return True
+def is_tiktok_url(text):
+    parsed = urlparse(text)
+    return bool(parsed.netloc) and parsed.netloc == "www.tiktok.com"
+
+
+def strip_url_params(url):
+    parsed = urlparse(url)
+    return parsed.scheme + "://" + parsed.netloc + parsed.path
 
 
 def download_video(url):
@@ -39,9 +45,3 @@ def download_video(url):
     full_file_path = VIDEO_OUTPUT_DIR + file_name
     logger.info("Full video path: %s", full_file_path)
     return full_file_path
-
-
-def convert_to_gif():
-    # todo implement
-    p = subprocess.run("ffmpeg", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    logger.info(p)
